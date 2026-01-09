cfd789193d180499309970dd60c44e3f2d82c1bd
riyaaa15 <riyaredhu1020@gmail.com>
2026-01-09 18:02:01 +0530

    image gallery

---
+++ image gallery/app.js
@@ -0,0 +1,18 @@
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.querySelector("#backBtn");
+let nextBtn  = document.querySelector("#nextBtn");
+
+scrollContainer.addEventListener("wheel", (evt)=> {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+});
+
+nextBtn.addEventListener("click", ()=>{
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft += 900;
+});
+
+backBtn.addEventListener("click", ()=> {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft -= 900;
+});
